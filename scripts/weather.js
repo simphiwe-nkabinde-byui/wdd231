f9b4bc42ac84ede0ae52a6c5de1fde2f2ec467b0
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-03-27 19:58:21 +0200

    W02 Consuming an API - weather api activity

---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+const weatherIcon = document.querySelector('#weather-icon');
+const caption = document.querySelector('figcaption');
+const currTemp = document.querySelector('#current-temp');
+
+const lat = 49.75
+const lon = 6.64
+const apiKey = '36d682e19df5d55ae8ea9cb18436acbd'
+const units = 'metric'
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            
+            displayResults(data);
+        } else {
+            throw Error(await response.text())
+        }
+    } catch (error) {
+        console.log({error});
+    }
+}
+
+function displayResults(data) {
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    weatherIcon.alt = description
+    currTemp.innerHTML = `${data.main.temp} &deg;C`
+    caption.textContent = data.weather[0].description
+}
+apiFetch();
