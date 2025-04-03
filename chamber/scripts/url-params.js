0d4d35124c6dc1ba7089a20356881917245ffd6f
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-04-03 21:30:10 +0200

    fix issues

--- chamber/scripts/url-params.js
+++ chamber/scripts/url-params.js
@@ -5,8 +5,8 @@ results.innerHTML = `
 <p><strong>Name:</strong> ${searchParams.get('firstname')} ${searchParams.get('lastname')}</p>
 <p><strong>Email:</strong> ${searchParams.get('email')}</p>
 <p><strong>Phone:</strong> ${searchParams.get('phone')}</p>
-<p><strong>Organisation:</strong> ${searchParams.get('organisation-name')}</p>
-<p><strong>Organisational Title:</strong> ${searchParams.get('organisation-title')}</p>
+<p><strong>Organisation:</strong> ${searchParams.get('organization-name')}</p>
+<p><strong>Organisational Title:</strong> ${searchParams.get('organization-title')}</p>
 <p><strong>Organisation Description:</strong> ${searchParams.get('description')}</p>
 <p><strong>Membership Level:</strong> ${searchParams.get('membership-level')}</p>
 <p><strong>Submitted at:</strong> ${searchParams.get('timestamp')}</p>
